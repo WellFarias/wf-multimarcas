2bb2dddf4612ab7e981edcbb58c6d89edb05981f
Well Farias <well.farias1969@gmail.com>
2020-11-26 11:09:45 -0300

    inclusao componente AppBar

---
+++ src/components/AppBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app-bar color="#000000">
+    <div style="height:100%; display: flex; margin: auto;">
+
+        <img src="/wf-logo.png" alt="" style="width: 200px; margin-left: -20px" >
+    <div style="margin-left: 840px">
+        <v-btn class="btns" elevation="0" style="color: white; background-color: black; height: 100%;"  >Home</v-btn>
+        <v-btn class="btns" elevation="0" style="color: white; background-color: black; height: 100%;" >Veiculos</v-btn>
+        <v-btn class="btns" elevation="0" style="color: white; background-color: black; height: 100%;" >Contato</v-btn>
+
+    </div>
+
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <AppBar/>
+</template>
+
+<script>
+import AppBar from "./AppBar"
+export default {
+    components: { AppBar }
+}
+</script>
+
+<style>
+
+</style>
