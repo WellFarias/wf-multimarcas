368eaefb576aed6f55353a88906981c6c4a87ff2
Well Farias <well.farias1969@gmail.com>
2021-01-05 14:52:00 -0300

    mobile

--- src/components/TelaVeiculos/Cards.vue
+++ src/components/TelaVeiculos/Cards.vue
@@ -20,9 +20,9 @@
               <v-img :src="carro.fotos? carro.fotos[0] : ''" height="300px"></v-img>
               <v-card-title>{{carro.marca}} - {{carro.nome}}</v-card-title>
               <v-card-subtitle style="margin-top: 3px">
-                <h6>
+                <h4>
                   <strong>R$ {{carro.preco}}</strong>
-                </h6>
+                </h4>
                 <p>Motor: {{carro.motor}}</p>
                 <p>Ano: {{carro.ano}}</p>
                 <p>Câmbio: {{carro.cambio}}</p>
@@ -46,10 +46,10 @@
           <v-card-title>
             <h4>{{carro.marca}} - {{ carro.nome }}</h4>
           </v-card-title>
-          <v-carousel hide-delimiters>
+          <v-carousel :continuous="false" :height="height" hide-delimiters>
             <v-carousel-item v-for="(foto, i) in carro.fotos" :key="i" :src="foto"></v-carousel-item>
           </v-carousel>
-          <v-card-text>
+          <v-card-text class="pa-0">
             <v-container>
               <v-row style="margin-top: 10px">
                 <v-col></v-col>
@@ -59,98 +59,99 @@
                 <v-col cols="2"></v-col>
               </v-row>
 
+                  <v-divider></v-divider>
               <v-row>
                 <v-col>
-                  <h6 class="my-1 center">Ano</h6>
+                  <h3 class="my-1 center">Ano</h3>
                   <p class="my-1 center">{{ carro.ano }}</p>
                 </v-col>
                 <v-divider vertical></v-divider>
                 <v-col>
-                  <h6 class="my-1 center">Portas</h6>
+                  <h3 class="my-1 center">Portas</h3>
                   <p class="my-1 center">{{ carro.porta }}</p>
                 </v-col>
                 <v-divider vertical></v-divider>
                 <v-col>
-                  <h6 class="my-1 center">Km</h6>
+                  <h3 class="my-1 center">Km</h3>
                   <p class="my-1 center">{{ carro.km }}</p>
                 </v-col>
               </v-row>
               <hr />
               <v-row>
                 <v-col>
-                  <h6 class="my-1 center">Potência do motor</h6>
+                  <h3 class="my-1 center">Potência do motor</h3>
                   <p class="my-1 center">{{ carro.motor }}</p>
                 </v-col>
                 <v-divider vertical></v-divider>
                 <v-col>
-                  <h6 class="my-1 center">Combustivel</h6>
+                  <h3 class="my-1 center">Combustivel</h3>
                   <p class="my-1 center">{{ carro.combustivel }}</p>
                 </v-col>
                 <v-divider vertical></v-divider>
                 <v-col>
-                  <h6 class="my-1 center">Cor</h6>
+                  <h3 class="my-1 center">Cor</h3>
                   <p class="my-1 center">{{ carro.cor }}</p>
                 </v-col>
               </v-row>
-              <hr />
+              <v-divider></v-divider>
               <v-row>
                 <v-col>
-                  <h6 class="my-1 center">Câmbio do carro</h6>
+                  <h3 class="my-1 center">Câmbio do carro</h3>
                   <p class="my-1 center">{{ carro.cambio }}</p>
                 </v-col>
               </v-row>
-              <hr />
-              <v-row>
-                <h4 class="center" style="color: black">
+              <v-divider></v-divider>
+              <v-row justify="center">
+                <h2 class="mt-6 mb-6 mx-4" style="color: black">
                   Entre em contato com a gente ou vá até a loja, para fazer
                   o finânciamento!
-                </h4>
+                </h2>
               </v-row>
-              <hr />
+              <v-divider></v-divider>
               <v-row>
-                <p class="my-1">Endereço: Rua Raimundo Carneiro 123</p>
+                <p class="my-1 mt-6 ml-4"><strong>Endereço:</strong> Rua Raimundo Carneiro 123</p>
               </v-row>
               <v-row>
-                <p class="my-1">
-                  Funcionamento: Segunda à Sexta | 9h00 às 18h00 e aos
+                <p class="my-1 ml-4 mb-6">
+                  <strong>Funcionamento:</strong> Segunda à Sexta | 9h00 às 18h00 e aos
                   Sábados | 9h00 às 15h00
                 </p>
               </v-row>
-              <hr />
+              <v-divider></v-divider>
               <v-row>
                 <v-col xs="12">
-                  <p class="my-1">Contatos:</p>
+                  <p class="my-1 ml-lg-1"><strong>Contatos:</strong></p>
                 </v-col>
               </v-row>
-              <v-col xs="12">
-                <p class="my-1">
-                  Fulao: (11) 945824785
+              <v-col xs="12" >
+                <p class="ml-n2"><strong>Beethoven:</strong></p>
                   <v-btn
-                    class="mx-4 black--text"
+                    class="ml-n3"
                     href="https://api.whatsapp.com/send/?phone=5511945290686&text=Olá+tudo+bem?&app_absent=0"
-                    icon
+                    color="green"
+                    outlined
                   >
-                    <v-icon size="20px">mdi-whatsapp</v-icon>
+                    (11) 945824785
+                    <v-icon class="ml-2" size="20px">mdi-whatsapp</v-icon>
                   </v-btn>
-                </p>
               </v-col>
               <v-col xs="12">
-                <p class="my-1">
-                  Roberto: (11) 40028922
+                <p class="ml-n2"><strong>Roberto:</strong></p>
                   <v-btn
-                    class="mx-4 black--text"
+                  class="ml-n3"
                     href="https://api.whatsapp.com/send/?phone=5511945290686&text=Olá+tudo+bem?&app_absent=0"
-                    icon
+                    color="green"
+                    outlined
                   >
-                    <v-icon size="20px">mdi-whatsapp</v-icon>
+                   (11) 40028922
+                    <v-icon class="ml-2" size="20px">mdi-whatsapp</v-icon>
                   </v-btn>
-                </p>
               </v-col>
             </v-container>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="red" style="color: white" large @click="dialog = false">Ok</v-btn>
+            <v-btn color="red" style="color: white" @click="dialog = false">Ok</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -175,6 +176,16 @@ export default {
       carros: "carros",
       carro: "carro"
     }),
+     height (height) {
+        switch (this.$vuetify.breakpoint.name) {
+          case 'xs': return 250
+          case 'sm': return 400
+          case 'md': return 500
+          case 'lg': return 600
+          case 'xl': return 800
+        }
+        return height
+      },
 
   },
 
